df3250aa8f6e4532046b5765773a5a431ec357f5
Ezequiel74452 <ezequielcaviglione@hotmail.com>
2024-05-22 17:53:38 -0300

    Fix etapa 5

--- app/src/main/java/ayds/songinfo/moredetails/domain/OtherInfoRepository.kt
+++ app/src/main/java/ayds/songinfo/moredetails/domain/OtherInfoRepository.kt
@@ -1,5 +1,5 @@
 package ayds.songinfo.moredetails.domain
 
-interface Repository{
+interface OtherInfoRepository{
     fun getArtistInfo(artistName: String): ArtistBiography;
 }
--- app/src/main/java/ayds/songinfo/moredetails/presenter/ArtistBiographyDescrptionHelperTest.kt
+++
@@ -1,4 +0,0 @@
-class ArtistBiographyDescriptionHelperTest{
-    private val artistBiographyDescriptionHelper: ArtistBiographyDescriptionHelper = 
-    
-}
